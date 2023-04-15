e2b3f20bfa1e72c523a1eb0fcffd6b77812122e6
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-04-14 21:13:41 -0400

    app routes implemented

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -2,9 +2,32 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-	return res.json({ message: "Hello world Ignite!" });
+/* GET - Busca uma informação dentro do servidor
+   POST - Inserir uma informação no servidor 
+	 PUT - Alterar uma informação no servidor
+	 PATCH - Alterar uma informação especifica no servidor 
+	 DELETE - Deletar uma informação no servidor
+*/
+	
+app.get('/courses', (req, res) => {
+	return response.json(['Curso 1', 'Curso 2', 'Curso 3']);
+})
+
+app.post('/courses', (req, res) => {
+	return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']);
 });
 
-const port = 4000;
+app.put('/courses/:id', (req, res) => {
+	return response.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4']);
+});
+
+app.patch('/courses/:id', (req, res) => {
+	return response.json(['Curso 6', 'Curso 7', 'Curso 3', 'Curso 4']);
+});
+
+app.delete('/courses/:id', (req, res) => {
+	return response.json(['Curso 6', 'Curso 7', 'Curso 4']);
+})
+
+const port = 3000;
 app.listen(port);
